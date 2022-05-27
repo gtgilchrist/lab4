384cc8bddf78de4c670e37e6ea2319db6d1d6266
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-26 21:00:53 -0700

    Uploading to git

--- ext2-create.c
+++ ext2-create.c
@@ -423,9 +423,9 @@ void write_inode_table(int fd) {
 	symlink_hello_world_inode.i_gid = 1000;
 	symlink_hello_world_inode.i_links_count = 1;
 	symlink_hello_world_inode.i_blocks = 0;
-	symlink_hello_world_inode.i_block[0] = ASCII_HELL;
-	symlink_hello_world_inode.i_block[1] = ASCII_O_OWO;
-    symlink_hello_world_inode.i_block[2] = ASCII_RLD;
+	symlink_hello_world_inode.i_block[0] = 0x68656C6C;
+	symlink_hello_world_inode.i_block[1] = 0x6F2D776F;
+    symlink_hello_world_inode.i_block[2] = 0x00726C64;
 	write_inode(fd, HELLO_INO, &symlink_hello_world_inode);
 }
 
