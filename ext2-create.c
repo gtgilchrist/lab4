84cd65eda5778e1ea98f69be97530f80a22aa64f
Grant Gilchrist <gilchrist@ucla.edu>
2022-05-26 17:12:49 -0700

    Uploading to git

--- ext2-create.c
+++ ext2-create.c
@@ -285,7 +285,11 @@ void write_block_bitmap(int fd) {
 	unsigned char *bitmap = calloc(NUM_BLOCKS, sizeof(unsigned char));
 	for(int i = 128; i < 1024; i++)
 		bitmap[i] = 0x00;
-	write(fd, &bitmap, BLOCK_SIZE);
+
+	ssize_t size = BLOCK_SIZE;
+	if(write(fd, &bitmap, size) != size){
+		errno_exit("write");
+	}
 	free(bitmap);
 }
 
